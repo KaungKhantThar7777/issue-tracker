77be61b3f3f9fc6a57e9a6d2b3d135695aa28918
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2022-12-29 08:57:19 +0630

    added search input

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,23 +1,56 @@
 import { useQuery } from "react-query";
 import { IssueItem } from "./IssueItem";
+import { useState } from "react";
 
 export default function IssuesList({ labels, status }) {
-  const { isLoading, data } = useQuery(["issues", { labels, status }], () => {
+  const [searchValue, setSearchValue] = useState("");
+  const issuesListQuery = useQuery(["issues", { labels, status }], () => {
     const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
     const statusString = status ? `&status=${status}` : "";
     return fetch(`/api/issues?${labelsString}${statusString}`).then((res) =>
       res.json()
     );
   });
+  const searchQuery = useQuery(
+    ["issues", "search", searchValue],
+    async () => {
+      return fetch(`/api/search/issues?q=${searchValue}`).then((res) =>
+        res.json()
+      );
+    },
+    {
+      enabled: !!searchValue,
+    }
+  );
+
+  console.log(searchQuery, searchValue);
 
   return (
     <div>
-      <h1>Issues List</h1>
-      {isLoading ? (
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setSearchValue(e.target.elements.search.value);
+        }}
+      >
+        <label htmlFor="search">Search Issues</label>
+        <input
+          type="search"
+          placeholder="Search"
+          id="search"
+          name="search"
+          onChange={(e) => {
+            if (e.target.value.length === 0) {
+              setSearchValue("");
+            }
+          }}
+        />
+      </form>
+      {issuesListQuery.isLoading ? (
         <p className="loading">Loading....</p>
-      ) : (
+      ) : !searchValue ? (
         <ul className="issues-list">
-          {data.map((issue) => (
+          {issuesListQuery.data.map((issue) => (
             <IssueItem
               key={issue.id}
               commentCount={issue.comments.length}
@@ -25,6 +58,25 @@ export default function IssuesList({ labels, status }) {
             />
           ))}
         </ul>
+      ) : (
+        <>
+          <h2>Search Results</h2>
+
+          {searchQuery.isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <ul className="issues-list">
+              <p>{searchQuery.data.count} Results</p>
+              {searchQuery.data?.items?.map((issue) => (
+                <IssueItem
+                  key={issue.id}
+                  commentCount={issue.comments.length}
+                  {...issue}
+                />
+              ))}
+            </ul>
+          )}
+        </>
       )}
     </div>
   );
